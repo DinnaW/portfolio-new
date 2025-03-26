cf7996f1370a583fe831ebba8e75d93ca0db4f10
Dinura Wanigasekara <dinura.20231495@iit.ac.lk>
2025-03-26 21:17:56 +0530

    paths

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio-new/', // Added trailing slash
+  base: process.env.VITE_BASE_PATH || "portfolio-new", // Added trailing slash
   plugins: [react()],
   resolve: {
     alias: {
